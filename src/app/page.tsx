3c6f4263766927640ae4ce63ac6d0748ce063328
Angel J. Haro <haro.j.angel@gmail.com>
2024-06-30 18:32:52 -0700

    photo + socials + stats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,53 @@
-import React from "react";
-import Head from "next/head";
-import TransitionEffect from "../components/TransitionEffect";
-import Layout from "./layout";
-// import Layout from '../components/Layout';
-import HorizantalSelfie from "@/components/selfie/HorizantalSelfie";
-import TextAnimation from "@/components/animation/TextAnimation";
-import ResumeContactButton from "../components/ResumeContactButton";
-import AnimatedStats from "../components/AnimatedStats";
-import LandingProject from "../components/LandingProject";
-import WrapperLandingProject from "../components/WrapperLandingProject";
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-const Home: React.FC = () => {
-  const alan = `Until you make the unconscious conscious, it will direct your life and you will call it fate. You are a function of what the whole universe is doing in the same way that a wave is a function of what the whole ocean is doing. ~ Alan Watts`;
-
+const Home = () => {
   return (
-    <>
-      <Head>
-        <title>aharoJ | Home</title>
-        <meta name="description" content="Created by aharoJ" />
-      </Head>
-      <TransitionEffect />
-
-      <main className="flex min-h-screen w-full items-center text-dark dark:text-light">
-        <Layout className="">
-          {/* Selfie | Name | Summary | Buttons */}
-          <div className="flex w-full md:flex-col md:items-center md:justify-center">
-            <div className="w-[37%] md:w-[100%]">
-              <HorizantalSelfie />
-            </div>
-            <div className="flex w-[63%] flex-col items-center self-center md:w-full">
-              <TextAnimation
-                text="Angel J. Haro"
-                className="mb-2 justify-center text-9xl font-light tracking-wider text-gray-400 
-                dark:text-orange-200 md:mb-4 md:flex-wrap md:text-6xl md:font-light "
-              />
-              <div className="flex w-[90%] items-center self-center md:mb-0 md:flex-wrap">
-                <p className="text-3xl font-normal md:font-light md:text-xl md:w-full">
-                  {alan}{" "}
-                </p>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div
+          className="flex flex-col xl:flex-row items-center justify-between 
+        xl:pt-8 xl:pb-24"
+        >
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl"> Software Developer </span>
+            <h1 className="h1">
+              Hello I am <br /> <span className="text-accent"> aharoJ </span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel in backend developemnt and i am proficient in various
+              programming languages and frameworks.
+            </p>
+            {/* btn and socials */}
+            <div className=" flex flex-col gap-8 xl:flex-row items-center">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span> Download CV </span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
-              <div className="my-4 flex w-[50%] md:w-[100%] md:my-12">
-                <ResumeContactButton />
-              </div>
             </div>
+          </div >
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo/>
           </div>
-
-          {/* React Icons */}
-          <div className="container-for-stats">
-            <AnimatedStats />
-          </div>
-
-          {/* LANDING PROJECT */}
-          <div className="landingproject-desktop md:hidden">
-            <LandingProject />
-          </div>
-          <div className="landingproject-iPhone hidden md:block">
-            <WrapperLandingProject />
-          </div>
-        </Layout>
-      </main>
-    </>
+        </div>
+      </div>
+      <Stats/>
+    </section>
   );
 };
 
