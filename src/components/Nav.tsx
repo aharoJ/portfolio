55d6f9b864060cbbac410ad7c97921af8c760a29
Angel J. Haro <haro.j.angel@gmail.com>
2024-06-30 18:16:54 -0700

    temp nav bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link"; // Correct import for Next.js links
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const Nav = () => {
+  const path_name = usePathname();
+  console.log(path_name);
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === path_name && "text-accent border-b-2 border-accent"
+              } capitalize font-medium hover:text-accent transition-all`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;
