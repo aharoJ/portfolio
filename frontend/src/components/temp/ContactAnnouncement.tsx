7b41b5a72eb8a9f2500915003b3771ef9aebb602
Angel J. Haro <haro.j.angel@gmail.com>
2024-07-23 23:05:05 -0700

    temp: Contact Announcement

---
+++ frontend/src/components/temp/ContactAnnouncement.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ContactAnnouncement = () => {
+  return (
+    <div className="container mx-auto h-full mt-14">
+      <h2 className="h2 justify-center flex "> NOTICE </h2>
+      <span className="text-green-300 text-3xl">Announcement: 07.23.2024</span>
+
+      <p>
+        • The &quot;Send Message&quot; has not yet been connected to an API.
+      </p>
+      <p>• Currently working on integrating it with an API.</p>
+    </div>
+  );
+};
+
+export default ContactAnnouncement;
