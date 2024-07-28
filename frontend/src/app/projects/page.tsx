b6c1ed9961c8a17de8c79459f6b53cbffafd9ff4
Angel J. Haro <haro.j.angel@gmail.com>
2024-07-27 21:46:52 -0700

    page: project page creation + component

---
+++ frontend/src/app/projects/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import twitter from "../../../public/projects/twitter.png";
+import FeaturedProject from "@/components/proj/FeaturedProject";
+
+const Featured: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>HELLOW </h1>
+      </div>
+
+      <div>
+        <FeaturedProject />
+      </div>
+    </>
+  );
+};
+
+export default Featured;
