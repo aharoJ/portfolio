28cfaef10141e64cc8e9a4e1fe84b0f16e239097
Angel J. Haro <haro.j.angel@gmail.com>
2024-07-27 20:21:29 -0700

    project: 50 | 50 balance

--- frontend/src/components/proj/FeaturedProject.tsx
+++ frontend/src/components/proj/FeaturedProject.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { GithubIcon } from "../svg/SvgIcons";
 
 const FramerImage = motion(Image);
 
@@ -13,7 +12,6 @@ interface BigProjectProps {
   title: string;
   description: string;
   img: StaticImageData;
-  // img: string;
   link: string;
   github: string;
 }
@@ -27,14 +25,14 @@ const BigProject: React.FC<BigProjectProps> = ({
   github,
 }) => {
   return (
-    <article className="relative flex w-full items-center rounded-3xl border border-solid border-dark bg-light p-4 lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
-      <div className="absolute -right-3 top-0 -z-10 h-[101%] w-[101%] rounded-[2.5rem] rounded-br-3xl bg-dark sm:h-[102%] xs:-right-2 xs:w-full xs:rounded-[1.5rem]" />
+    <article className="container mx-auto relative flex flex-col w-full items-center rounded-3xl border-2 border-solid border-dark bg-main p-4">
+      <div className="absolute -right-3 top-0 -z-10 h-[101%] w-[101%] rounded-[2.5rem] rounded-br-3xl bg-white" />
 
       <div className="flex w-full">
         <Link
           href={link}
           target="_blank"
-          className="w-auto cursor-pointer overflow-hidden rounded-lg lg:w-full"
+          className="w-[50%] h-full cursor-pointer overflow-hidden rounded-lg"
         >
           <FramerImage
             src={img}
@@ -44,24 +42,24 @@ const BigProject: React.FC<BigProjectProps> = ({
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.2 }}
             sizes="(max-width: 768px) 100vw,
-                      (max-width: 1200px) 50vw,
-                      50vw"
+                    (max-width: 1200px) 50vw,
+                    50vw"
           />
         </Link>
 
-        <div className="flex w-[50%] flex-col md:w-full">
-          <div className="ml-6 flex flex-wrap justify-start space-x-4">
+        <div className="flex w-[50%] flex-col p-4">
+          <div className="mx-4 flex flex-wrap justify-start space-x-2">
             {hashtags.map((tag, index) => (
-              <span key={index} className="text-2xl font-light text-dark/80">
+              <span key={index} className="text-base font-light text-dark/80">
                 #{tag}
               </span>
             ))}
           </div>
 
-          <div className="ml-6 flex w-auto flex-row">
+          <div className="flex w-full flex-row">
             <div className="w-1/2">
               <Link href={link} target="_blank">
-                <h2 className="my-2 text-left text-7xl hover:underline font-semibold text-gray-400 sm:text-sm">
+                <h2 className="my-2 text-left text-2xl hover:underline font-semibold text-gray-400">
                   {title}
                 </h2>
               </Link>
@@ -71,17 +69,10 @@ const BigProject: React.FC<BigProjectProps> = ({
                 <Link href={github} target="_blank" className="w-12">
                   <GithubIcon />
                 </Link>
-                <Link
-                  href={link}
-                  target="_blank"
-                  className="rounded-lg bg-dark p-2 text-2xl font-semibold text-light"
-                >
-                  Visit Project
-                </Link>
               </div>
             </div>
           </div>
-          <p className="my-2 px-6 text-2xl font-normal text-dark/80 sm:text-sm">
+          <p className="my-2 text-xl font-normal text-dark/80 sm:text-sm">
             {description}
           </p>
         </div>
@@ -89,6 +80,27 @@ const BigProject: React.FC<BigProjectProps> = ({
     </article>
   );
 };
+
+interface SvgIconProps extends React.SVGProps<SVGSVGElement> {
+  className?: string;
+}
+
+export const GithubIcon: React.FC<SvgIconProps> = ({ className, ...rest }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 512 512"
+    {...rest}
+    className={`w-full h-full ${className}`}
+  >
+    <path fill="none" d="M0 0h512v512H0z" />
+    <path
+      fill="currentColor"
+      d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 0 0 3.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 0 1-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0 0 25.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 0 1 5-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 0 1 112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 0 1 5 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 0 0 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32Z"
+    />
+  </svg>
+);
 
 const Featured: React.FC = () => {
   return (
