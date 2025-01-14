c371e19dd63e738c6ded5b2b05524b0d08c87eaf
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-13 21:31:08 -0800

    cleaned up dead code

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,18 +1,24 @@
-import SkillOverview from "@/components/acerca/SkillOverview";
-import Timeline from "@/components/acerca/Timeline";
+import SkillOverview from "@/components/clean/SkillNavigator";
+import Timeline from "@/components/clean/Timeline";
+
+import Education from "@/components/clean/education/Education";
 import AboutAnnouncement from "@/components/temp/AboutAnnouncement";
 import React from "react";
 
 const About = () => {
   return (
     <>
-      <div>
+      {/* <div>
         <SkillOverview />
-      </div>
+      </div> */}
 
-      <div>
+      <section>
+        <Education />
+      </section>
+
+      <section>
         <Timeline />
-      </div>
+      </section>
 
       {/* <h1 className="flex container justify-center text-9xl"> DIVIDER </h1> */}
       <AboutAnnouncement />
