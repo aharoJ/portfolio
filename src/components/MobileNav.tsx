d313185829be61de700439c77373088aee41536c
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-15 15:23:55 -0800

    fixing delay in mobile device, closing out after a link is selcted

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,18 +1,23 @@
 "use client";
-
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+  SheetTitle,
+} from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
 
 const links = [
   { name: "home", path: "/" },
-  // { name: "services", path: "/services" },
-  // { name: "resume", path: "/resume" },
-  // { name: "work", path: "/work" },
   { name: "about", path: "/about" },
   { name: "projects", path: "/projects" },
   { name: "contact", path: "/contact" },
+  // { name: "services", path: "/services" },
+  // { name: "resume", path: "/resume" },
+  // { name: "work", path: "/work" },
 ];
 
 const MobileNav: React.FC = () => {
@@ -23,28 +28,29 @@ const MobileNav: React.FC = () => {
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-primary" />
       </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        {/* logo */}
-        <div className="mt-32 mb-40 text-center text-2xl">
+
+      <SheetContent side="right" className="flex flex-col fixed z-50">
+        <SheetTitle className="mt-32 mb-8 text-center text-2xl">
           <Link href="/">
-            <h1 className="h1">
+            <h1 className="h1 text-white">
               aharo<span className="text-primary">J</span>
             </h1>
           </Link>
-        </div>
-        {/* nav */}
+        </SheetTitle>
+
         <nav className="flex flex-col justify-center items-center gap-8">
           {links.map((link, index) => (
-            <Link
-              href={link.path}
-              key={index}
-              className={`${
-                link.path === path_name &&
-                "text-primary border-b-2 border-primary"
-              } text-xl capitalize hover:text-primary/50 transition-all`}
-            >
-              {link.name}
-            </Link>
+            <SheetClose asChild key={index}>
+              <Link
+                href={link.path}
+                className={`${
+                  link.path === path_name &&
+                  "text-primary border-b-2 border-primary"
+                } text-xl capitalize hover:text-primary/50 transition-all`}
+              >
+                {link.name}
+              </Link>
+            </SheetClose>
           ))}
         </nav>
       </SheetContent>
