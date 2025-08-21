1bbf3b105fbe938737b59f38b9f99d0c354bbd9e
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-21 13:38:29 -0700

    tmp

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,41 +1,48 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// @/TEST/(home)/page.tsx
+// path: @/app/about/page.tsx
+import React from "react";
 
-import LandingAnimatedStatsDesktop from "@/modules/home/LandingAnimatedStatsDesktop";
-import LandingAnimatedStatsMobile from "@/modules/home/LandingAnimatedStatsMobile";
-import LandingAnimatedStatsSwipeMobile from "@/modules/home/LandingAnimatedStatsSwipeMobile";
-import LandingDesktop from "@/modules/home/LandingDesktop";
-import LandingMobile from "@/modules/home/LandingMobile";
-import SkillsDesktop from "@/modules/home/SkillsDesktop";
-import AnimateStatsDesktop from "@/modules/tempToDELETE/homes/AnimateStatsDesktop";
+import EducationDesktop from "@/modules/education/EducationDesktop";
+import ExperienceDesktop from "@/modules/experience/ExperienceDesktop";
+import EducationMobile from "@/modules/education/EducationMobile";
+import ExperienceMobile from "@/modules/experience/ExperienceMobile";
+import SkillsMobile from "@/modules/skills/SkillsMobile";
+import SkillsTestingggDesktop from "@/modules/skills/SkillsTestingggDesktop";
+import SkillsDesktop from "@/modules/skills/SkillsDesktop";
 
 const Test = () => {
   return (
     <>
-      <main>
-        {/* --- --- --- --- HERO PAGE --- --- --- --- */}
-        <section aria-label="SOCIAL ICONS">
-          <div className="hidden lg:block">
-            <LandingDesktop />
-          </div>
-          <div className="block lg:hidden">
-            <LandingMobile />
-          </div>
-        </section>
-        {/* --- --- --- --- ... .... ... --- --- --- --- */}
+      {/* -------------------- Education Section -------------------- */}
+      <section title="EDUCATION">
+        <div className="hidden lg:block">
+          <EducationDesktop />
+        </div>
+        <div className="block lg:hidden">
+          <EducationMobile />
+        </div>
+      </section>
 
-        {/* -------------------- animate stats -------------------- */}
-        <section aria-label="ANIMATED STATS">
-          <div className="hidden lg:block">
-            <LandingAnimatedStatsDesktop />
-          </div>
-          <div className="block lg:hidden">
-            {/* <LandingAnimatedStatsMobile /> */}
-            <LandingAnimatedStatsSwipeMobile/>
-          </div>
-        </section>
-        {/* --- --- --- --- ... .... ... --- --- --- --- */}
-      </main>
+      {/* -------------------- Experience Section -------------------- */}
+      <section title="EXPERIENCE">
+        <div className="hidden lg:block">
+          <ExperienceDesktop />
+        </div>
+        <div className="block lg:hidden">
+          <ExperienceMobile />
+        </div>
+      </section>
+
+      {/* -------------------- Skills Section -------------------- */}
+      <section title="ANIMATED STATS">
+        <div className="hidden lg:block">
+          <SkillsDesktop />
+          <SkillsTestingggDesktop />
+        </div>
+        <div className="block lg:hidden">
+          <SkillsMobile />
+        </div>
+      </section>
+      {/* --- --- --- --- ... .... ... --- --- --- --- */}
     </>
   );
 };
