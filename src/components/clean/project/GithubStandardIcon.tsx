428ad0911b48f8ae7af619e5eeb7c07bac58f86f
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-25 14:38:36 -0700

    temp

--- src/components/clean/project/GithubStandardIcon.tsx
+++ src/components/clean/project/GithubStandardIcon.tsx
@@ -1,3 +1,5 @@
+// @/components/clean/project/GithubStandardIcon.tsx
+
 import Link from "next/link";
 import React from "react";
 import { FaGithub } from "react-icons/fa";
@@ -11,14 +13,13 @@ const GithubStandardIcon: React.FC<SocialProps> = ({ path }) => {
     <div className="flex space-x-4 lg:space-x-8">
       <Link
         href={path}
-        className="lg:w-7 lg:h-7 w-6 h-6 border border-primary rounded-full flex justify-center items-center text-primary 
-        text-xl hover:bg-primary hover:text-red-600 hover:transition-all duration-500"
+        className="lg:w-7 lg:h-7 w-6 h-6  rounded-full flex justify-center items-center  
+        text-xl hover:bg-soft-moss/40  hover:transition-all duration-500"
         target="_blank"
       >
-        <FaGithub className="text-3xl text-gray-400 hover:text-black" />
+        <FaGithub className="text-3xl text-creamy-sage " />
       </Link>
     </div>
   );
 };
-
 export default GithubStandardIcon;
