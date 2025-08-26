944171954ce3773a8490f532f0397d22ce3a3668
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-26 12:13:40 -0700

    temp to delete

--- src/modules/tempToDELETE/homes/HomeGoToProjectsBtn.tsx
+++ src/modules/tempToDELETE/homes/HomeGoToProjectsBtn.tsx
@@ -5,7 +5,7 @@ import { GoProjectSymlink } from "react-icons/go";
 const HomeGoToProjectsBtn = () => {
   return (
     <a
-      href="https://www.aharoj.io/projects" // Change this to the path of your PDF
+      href="https://www.aharoj.io/projects"
       target="_self" // Optional: _blank | _parent | _self | _top
       rel="noopener noreferrer" // Optional: Adds security for external links
     >
